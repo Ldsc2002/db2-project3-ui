bf015837aa3d7d892fbe06c3a503f2825ac13a03
Ldsc2002 <luissantoscuellar@gmail.com>
2023-05-22 11:34:08 -0600

    Basic nav in admin panel

--- src/components/adminPanel/AdminPanel.jsx
+++ src/components/adminPanel/AdminPanel.jsx
@@ -1,10 +1,41 @@
-import React from 'react'
+import React, { useState } from 'react'
 import classes from './AdminPanel.module.css'
 
+const options = [
+    "Add node with label 'User'",
+    "Add node with multiple labels",
+    "View all nodes",
+    "Update node with label 'User'",
+    "Create relationship between user and job",
+    "Delete node with label 'User'",
+    "Delete relationship between user and job",
+    "Delete property from node with label 'User'",
+    "Delete property from relationship between user and job"
+]
+
 function AdminPanel() {
+    const [page, setPage] = useState(options[0])
+
     return (
         <div className={classes.container}>
-            <h1>Admin Panel</h1>
+            <div className={classes.buttonContainer}>
+                {options.map((option) => (
+                    <button 
+                        className={classes.navButton} 
+                        key={option} type="button" 
+                        onClick={() => setPage(option)}
+                        style={{outline: `${page === option ? '3px solid black' : 'none'}`}}
+                    >
+                        {option}
+                    </button>
+                ))}
+            </div>
+
+            <div className={classes.pageContainer}>
+                <h1>{page}</h1>
+
+            </div>
+
         </div>
     )
 }
